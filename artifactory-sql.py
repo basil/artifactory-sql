0830319041db11083d3435e6142138feccb53d12
Basil Crow <me@basilcrow.com>
2025-06-17 12:16:26 -0700

    Update for 2025

--- artifactory-sql.py
+++ artifactory-sql.py
@@ -7,13 +7,15 @@ import sqlite3
 def import_data(input_files, output):
     with sqlite3.connect(output, isolation_level="DEFERRED") as db:
         c = db.cursor()
+        # https://jfrog.com/help/r/artifactory-how-to-debug-artifactory-issues-based-on-http-status-codes/request-log
         c.execute(
-            "CREATE TABLE IF NOT EXISTS logs(date_timestamp INTEGER, hash TEXT, request_time_ms INTEGER, request_type TEXT, ip TEXT, repo TEXT, path TEXT, size_bytes INTEGER) STRICT"
+            "CREATE TABLE IF NOT EXISTS logs(date_timestamp INTEGER, trace_id TEXT, remote_address TEXT, username TEXT, request_method TEXT, request_url TEXT, return_status INTEGER, request_content_length_bytes INTEGER, response_content_length_bytes INTEGER, request_duration_ms INTEGER, request_user_agent TEXT) STRICT"
         )
         c.execute("""PRAGMA synchronous = OFF""")
         c.execute("""PRAGMA journal_mode = OFF""")
         for f in input_files:
             parse_file(f, c)
+        db.commit()
 
 
 def parse_file(input_file, c):
@@ -22,38 +24,47 @@ def parse_file(input_file, c):
         for line in f:
             line = line.strip()
             parts = line.split("|")
-            assert len(parts) == 7, line
+            assert len(parts) == 11, line
             date_timestamp_raw = parts[0]
-            date_timestamp = datetime.datetime.strptime(
-                date_timestamp_raw, "%Y%m%d%H%M%S"
-            ).timestamp()
-            hash = parts[1]
-            request_time_ms = int(parts[2])
-            request_type = parts[3]
-            assert request_type in [
-                "UPLOAD",
-                "DOWNLOAD",
-                "REQUEST",
-                "REDIRECT",
+            date_timestamp = int(
+                datetime.datetime.fromisoformat(
+                    date_timestamp_raw.replace("Z", "+00:00")
+                ).timestamp()
+            )
+            trace_id = parts[1]
+            remote_address = parts[2]
+            username = parts[3]
+            request_method = parts[4]
+            assert request_method in [
+                "DELETE",
+                "GET",
+                "HEAD",
+                "OPTIONS",
+                "PATCH",
+                "POST",
+                "PROPFIND",
+                "PUT",
             ], line
-            ip = parts[4]
-            resource = parts[5]
-            resource_parts = resource.split(":", 1)
-            assert len(resource_parts) == 2, line
-            repo = resource_parts[0]
-            path = resource_parts[1]
-            size_bytes = int(parts[6])
+            request_url = parts[5]
+            return_status = int(parts[6])
+            request_content_length_bytes = int(parts[7])
+            response_content_length_bytes = int(parts[8])
+            request_duration_ms = int(parts[9])
+            request_user_agent = parts[10]
             c.execute(
-                "INSERT INTO logs VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                "INSERT INTO logs VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                 (
                     date_timestamp,
-                    hash,
-                    request_time_ms,
-                    request_type,
-                    ip,
-                    repo,
-                    path,
-                    size_bytes,
+                    trace_id,
+                    remote_address,
+                    username,
+                    request_method,
+                    request_url,
+                    return_status,
+                    request_content_length_bytes,
+                    response_content_length_bytes,
+                    request_duration_ms,
+                    request_user_agent,
                 ),
             )
 
