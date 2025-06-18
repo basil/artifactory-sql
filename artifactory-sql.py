ae7977aad2531de2d9531c3e50ed8ba336fa864c
Basil Crow <me@basilcrow.com>
2025-06-18 11:47:10 -0700

    Simplify

--- artifactory-sql.py
+++ artifactory-sql.py
@@ -1,33 +1,34 @@
 import argparse
-import csv
 import datetime
 import geoip2.database
-import io
-import ipaddress
-import json
 import os
 import sqlite3
-import urllib.request
 
 
 class LogImporter:
-    def __init__(self, output, mmdb):
+    def __init__(self, output, asn, city):
         self.output = output
-        self.mmdb = mmdb
-        self.region_cache = {}
+        self.asn_mmdb = asn
+        self.city_mmdb = city
 
     def __enter__(self):
         self.db = sqlite3.connect(self.output)
         self.cursor = self.db.cursor()
-        if self.mmdb:
-            self.reader = geoip2.database.Reader(self.mmdb)
+        if self.asn_mmdb:
+            self.asn_reader = geoip2.database.Reader(self.asn_mmdb)
         else:
-            self.reader = None
+            self.asn_reader = None
+        if self.city_mmdb:
+            self.city_reader = geoip2.database.Reader(self.city_mmdb)
+        else:
+            self.city_reader = None
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        if self.reader:
-            self.reader.close()
+        if self.city_reader:
+            self.city_reader.close()
+        if self.asn_reader:
+            self.asn_reader.close()
         self.db.close()
 
     def setup_database(self):
@@ -42,104 +43,8 @@ class LogImporter:
         self.cursor.execute("PRAGMA journal_mode = OFF")
         self.db.commit()
 
-    def fetch_azure_ip_ranges(self):
-        with urllib.request.urlopen(
-            "https://raw.githubusercontent.com/femueller/cloud-ip-ranges/b2f09e245369a4d3f96de2b3faf4a47217537a7b/microsoft-azure-ip-ranges.json"
-        ) as response:
-            if response.getcode() != 200:
-                raise urllib.error.URLError(
-                    f"Unexpected HTTP status code {response.getcode()}"
-                )
-            return json.loads(response.read().decode("utf-8"))
-
-    def fetch_aws_ip_ranges(self):
-        with urllib.request.urlopen(
-            "https://ip-ranges.amazonaws.com/ip-ranges.json"
-        ) as response:
-            if response.getcode() != 200:
-                raise urllib.error.URLError(
-                    f"Unexpected HTTP status code {response.getcode()}"
-                )
-            return json.loads(response.read().decode("utf-8"))
-
-    def fetch_do_ip_ranges(self):
-        ip_ranges = []
-        with urllib.request.urlopen(
-            "https://raw.githubusercontent.com/femueller/cloud-ip-ranges/refs/heads/master/digitalocean.csv"
-        ) as response:
-            if response.getcode() != 200:
-                raise urllib.error.URLError(
-                    f"Unexpected HTTP status code {response.getcode()}"
-                )
-            csv_content = response.read().decode("utf-8")
-            csv_file = io.StringIO(csv_content)
-            reader = csv.reader(csv_file)
-            for row in reader:
-                assert len(row) == 5
-                network, country, region, city, postal = row[:5]
-                ip_ranges.append(
-                    {
-                        "network": network,
-                        "country": country,
-                        "region": region,
-                        "city": city,
-                        "postal": postal,
-                    }
-                )
-        return ip_ranges
-
-    def fetch_gcp_ip_ranges(self):
-        with urllib.request.urlopen(
-            "https://www.gstatic.com/ipranges/cloud.json"
-        ) as response:
-            if response.getcode() != 200:
-                raise urllib.error.URLError(
-                    f"Unexpected HTTP status code {response.getcode()}"
-                )
-            return json.loads(response.read().decode("utf-8"))
-
-    def find_azure_region(self, remote_address):
-        addr = ipaddress.ip_address(remote_address)
-        for value in self.azure_ip_ranges["values"]:
-            for prefix in value["properties"]["addressPrefixes"]:
-                if ipaddress.ip_address(remote_address) in ipaddress.ip_network(
-                    prefix
-                ):
-                    return value["properties"]["region"]
-        return None
-
-    def find_aws_region(self, remote_address):
-        addr = ipaddress.ip_address(remote_address)
-        for prefix in self.aws_ip_ranges["prefixes"]:
-            if addr in ipaddress.ip_network(prefix["ip_prefix"]):
-                return prefix["region"]
-        return None
-
-    def find_do_region(self, remote_address):
-        addr = ipaddress.ip_address(remote_address)
-        for prefix in self.do_ip_ranges:
-            if addr in ipaddress.ip_network(prefix["network"]):
-                return prefix["region"]
-        return None
-
-    def find_gcp_region(self, remote_address):
-        addr = ipaddress.ip_address(remote_address)
-        for prefix in self.gcp_ip_ranges["prefixes"]:
-            if "ipv6Prefix" in prefix:
-                effective_prefix = prefix["ipv6Prefix"]
-            else:
-                effective_prefix = prefix["ipv4Prefix"]
-            if addr in ipaddress.ip_network(effective_prefix):
-                return prefix["scope"]
-        return None
-
     def import_files(self, input_files):
         self.setup_database()
-        if self.reader:
-            self.azure_ip_ranges = self.fetch_azure_ip_ranges()
-            self.aws_ip_ranges = self.fetch_aws_ip_ranges()
-            self.do_ip_ranges = self.fetch_do_ip_ranges()
-            self.gcp_ip_ranges = self.fetch_gcp_ip_ranges()
         for f in input_files:
             self.parse_file(f)
 
@@ -147,87 +52,81 @@ class LogImporter:
         print("Parsing " + input_file)
         with open(input_file, "r") as f:
             for line in f:
-                line = line.strip()
-                parts = line.split("|")
-                assert len(parts) == 11, line
-                date_timestamp_raw = parts[0]
-                date_timestamp = int(
-                    datetime.datetime.fromisoformat(
-                        date_timestamp_raw.replace("Z", "+00:00")
-                    ).timestamp()
-                )
-                trace_id = parts[1]
-                remote_address = parts[2]
-                remote_organization = None
-                if self.reader:
-                    try:
-                        remote_organization = self.reader.asn(
-                            remote_address
-                        ).autonomous_system_organization
-                    except geoip2.errors.AddressNotFoundError:
-                        pass
-                remote_region = None
-                if remote_organization:
-                    if remote_address in self.region_cache:
-                        remote_region = self.region_cache[remote_address]
-                    elif (
-                        "azure" in remote_organization.lower()
-                        or "microsoft" in remote_organization.lower()
-                    ):
-                        remote_region = self.find_azure_region(remote_address)
-                        self.region_cache[remote_address] = remote_region
-                    elif (
-                        "aws" in remote_organization.lower()
-                        or "amazon" in remote_organization.lower()
-                    ):
-                        remote_region = self.find_aws_region(remote_address)
-                        self.region_cache[remote_address] = remote_region
-                    elif "digitalocean" in remote_organization.lower():
-                        remote_region = self.find_do_region(remote_address)
-                        self.region_cache[remote_address] = remote_region
-                    elif (
-                        "gcp" in remote_organization.lower()
-                        or "google" in remote_organization.lower()
-                    ):
-                        remote_region = self.find_gcp_region(remote_address)
-                        self.region_cache[remote_address] = remote_region
-                username = parts[3]
-                request_method = parts[4]
-                assert request_method in [
-                    "DELETE",
-                    "GET",
-                    "HEAD",
-                    "OPTIONS",
-                    "PATCH",
-                    "POST",
-                    "PROPFIND",
-                    "PUT",
-                ], line
-                request_url = parts[5]
-                return_status = int(parts[6])
-                request_content_length_bytes = int(parts[7])
-                response_content_length_bytes = int(parts[8])
-                request_duration_ms = int(parts[9])
-                request_user_agent = parts[10]
-                self.cursor.execute(
-                    "INSERT INTO logs VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                    (
-                        date_timestamp,
-                        trace_id,
-                        remote_address,
-                        remote_organization,
-                        remote_region,
-                        username,
-                        request_method,
-                        request_url,
-                        return_status,
-                        request_content_length_bytes,
-                        response_content_length_bytes,
-                        request_duration_ms,
-                        request_user_agent,
-                    ),
-                )
+                self.parse_line(line)
         self.db.commit()
+
+    def parse_line(self, line):
+        line = line.strip()
+        parts = line.split("|")
+        assert len(parts) == 11, line
+        date_timestamp_raw = parts[0]
+        date_timestamp = int(
+            datetime.datetime.fromisoformat(
+                date_timestamp_raw.replace("Z", "+00:00")
+            ).timestamp()
+        )
+        trace_id = parts[1]
+        remote_address = parts[2]
+        remote_organization = None
+        if self.asn_reader:
+            try:
+                remote_organization = self.asn_reader.asn(
+                    remote_address
+                ).autonomous_system_organization
+            except geoip2.errors.AddressNotFoundError:
+                pass
+        username = parts[3]
+        remote_region = None
+        if self.city_reader:
+            try:
+                city_data = self.city_reader.city(remote_address)
+                remote_region = city_data.country.iso_code
+                if (
+                    city_data.country.iso_code == "US"
+                    and city_data.subdivisions.most_specific.iso_code
+                ):
+                    remote_region += "/"
+                    remote_region += city_data.subdivisions.most_specific.iso_code
+                if city_data.city.name:
+                    remote_region += "/"
+                    remote_region += city_data.city.name
+            except geoip2.errors.AddressNotFoundError:
+                pass
+        request_method = parts[4]
+        assert request_method in [
+            "DELETE",
+            "GET",
+            "HEAD",
+            "OPTIONS",
+            "PATCH",
+            "POST",
+            "PROPFIND",
+            "PUT",
+        ], line
+        request_url = parts[5]
+        return_status = int(parts[6])
+        request_content_length_bytes = int(parts[7])
+        response_content_length_bytes = int(parts[8])
+        request_duration_ms = int(parts[9])
+        request_user_agent = parts[10]
+        self.cursor.execute(
+            "INSERT INTO logs VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            (
+                date_timestamp,
+                trace_id,
+                remote_address,
+                remote_organization,
+                remote_region,
+                username,
+                request_method,
+                request_url,
+                return_status,
+                request_content_length_bytes,
+                response_content_length_bytes,
+                request_duration_ms,
+                request_user_agent,
+            ),
+        )
 
 
 if __name__ == "__main__":
@@ -241,15 +140,21 @@ if __name__ == "__main__":
         help="The name of the SQLite database to create.",
     )
     parser.add_argument(
-        "-m",
-        "--mmdb",
+        "-a",
+        "--asn",
         default=None,
         help="Path to the GeoLite2 ASN MaxMind database file.",
+    )
+    parser.add_argument(
+        "-c",
+        "--city",
+        default=None,
+        help="Path to the GeoLite2 City MaxMind database file.",
     )
     parser.add_argument(
         "input", nargs="+", help="The Artifactory log file(s) to parse."
     )
     args = parser.parse_args()
-    with LogImporter(args.output, args.mmdb) as importer:
+    with LogImporter(args.output, args.asn, args.city) as importer:
         importer.import_files(args.input)
     os.execlp("sqlite3", "sqlite3", os.path.abspath(args.output))
