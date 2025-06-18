76c42916e2b69b04f1881f34ce885cd012e68d01
Basil Crow <me@basilcrow.com>
2025-06-18 09:52:35 -0700

    Detect AWS regions

--- artifactory-sql.py
+++ artifactory-sql.py
@@ -1,14 +1,18 @@
 import argparse
-import geoip2.database
 import datetime
+import geoip2.database
+import ipaddress
+import json
 import os
 import sqlite3
+import urllib.request
 
 
 class LogImporter:
     def __init__(self, output, mmdb):
         self.output = output
         self.mmdb = mmdb
+        self.region_cache = {}
 
     def __enter__(self):
         self.db = sqlite3.connect(self.output)
@@ -27,17 +31,36 @@ class LogImporter:
     def setup_database(self):
         # https://jfrog.com/help/r/artifactory-how-to-debug-artifactory-issues-based-on-http-status-codes/request-log
         self.cursor.execute(
-            "CREATE TABLE IF NOT EXISTS logs(date_timestamp INTEGER, trace_id TEXT, remote_address TEXT, remote_organization TEXT, username TEXT, request_method TEXT, request_url TEXT, return_status INTEGER, request_content_length_bytes INTEGER, response_content_length_bytes INTEGER, request_duration_ms INTEGER, request_user_agent TEXT) STRICT"
+            "CREATE TABLE IF NOT EXISTS logs(date_timestamp INTEGER, trace_id TEXT, remote_address TEXT, remote_organization TEXT, remote_region TEXT, username TEXT, request_method TEXT, request_url TEXT, return_status INTEGER, request_content_length_bytes INTEGER, response_content_length_bytes INTEGER, request_duration_ms INTEGER, request_user_agent TEXT) STRICT"
         )
         self.cursor.execute(
-            "CREATE INDEX IF NOT EXISTS idx_logs_search ON logs (request_url, response_content_length_bytes, remote_address, remote_organization)"
+            "CREATE INDEX IF NOT EXISTS idx_logs_search ON logs (request_url, response_content_length_bytes, remote_address, remote_organization, remote_region)"
         )
         self.cursor.execute("PRAGMA synchronous = OFF")
         self.cursor.execute("PRAGMA journal_mode = OFF")
         self.db.commit()
 
+    def fetch_aws_ip_ranges(self):
+        with urllib.request.urlopen(
+            "https://ip-ranges.amazonaws.com/ip-ranges.json"
+        ) as response:
+            if response.getcode() != 200:
+                raise urllib.error.URLError(
+                    f"Unexpected HTTP status code {response.getcode()}"
+                )
+            return json.loads(response.read().decode("utf-8"))
+
+    def find_aws_region(self, remote_address):
+        for prefix in self.aws_ip_ranges["prefixes"]:
+            if ipaddress.ip_address(remote_address) in ipaddress.ip_network(
+                prefix["ip_prefix"]
+            ):
+                return prefix["region"]
+        return None
+
     def import_files(self, input_files):
         self.setup_database()
+        self.aws_ip_ranges = self.fetch_aws_ip_ranges()
         for f in input_files:
             self.parse_file(f)
 
@@ -64,6 +87,17 @@ class LogImporter:
                         ).autonomous_system_organization
                     except geoip2.errors.AddressNotFoundError:
                         pass
+                remote_region = None
+                if remote_organization:
+                    if (
+                        "amazon" in remote_organization.lower()
+                        or "aws" in remote_organization.lower()
+                    ):
+                        if remote_address in self.region_cache:
+                            remote_region = self.region_cache[remote_address]
+                        else:
+                            remote_region = self.find_aws_region(remote_address)
+                            self.region_cache[remote_address] = remote_region
                 username = parts[3]
                 request_method = parts[4]
                 assert request_method in [
@@ -83,12 +117,13 @@ class LogImporter:
                 request_duration_ms = int(parts[9])
                 request_user_agent = parts[10]
                 self.cursor.execute(
-                    "INSERT INTO logs VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    "INSERT INTO logs VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                     (
                         date_timestamp,
                         trace_id,
                         remote_address,
                         remote_organization,
+                        remote_region,
                         username,
                         request_method,
                         request_url,
